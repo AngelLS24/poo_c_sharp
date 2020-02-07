45f6d00fec738b32ae1637b097678aec1890d9bf
AngelLS24 <angls2495@gmail.com>
2020-02-07 13:20:06 -0600

    Tarea 2

---
+++ Tareas/Calificaciones/Calificaciones/Alumno.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calificaciones
+{
+    class Alumno
+    {
+        public string Nombre { get; set; }
+        public string Ap_Paterno { get; set; }
+        public string Ap_Materno { get; set; }
+        public double[] Calificaciones { get; set; }
+        public double Calificacion { get; set; }
+        // Constructor
+        public Alumno(string nombre, string ap_paterno, string ap_materno, double [] calificaciones)
+        {
+            Nombre = nombre;
+            Ap_Paterno = ap_paterno;
+            Ap_Materno = ap_materno;
+            Calificacion = calculadora.califica(calificaciones);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Alumno {0} {1} {2} tiene {3:#.##} de calificacion final", Nombre, Ap_Paterno, Ap_Materno, Calificacion);
+        }
+    }
+
+    public static class calculadora
+    {
+        // Genera la calificacion de los alumnos
+        public static double califica(double [] calificaciones)
+        {
+            double res = 0;
+
+            res += calificaciones[0] * 0.6;
+            res += calificaciones[1] * 10 / 5 * 0.4;
+            if (calificaciones[2] > 5)
+            {
+                if (res < 10)
+                {
+                    if ((res + (calificaciones[2] * 0.08)) > 10)
+                        res = 10.0;
+                    else
+                        res += (calificaciones[2] * 0.08);
+                }
+                else
+                    return res;
+            }
+            return res;
+        }
+    }
+}
---
+++ Tareas/Calificaciones/Calificaciones/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Calificaciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cuantos alumnos se van a calificar: ");
+
+            int tam = Convert.ToInt32(Console.ReadLine());
+            string nom;
+            string ap_pa;
+            string ap_ma;
+
+            double[] calif = new double[3]; 
+
+            Alumno[] alumnos = new Alumno[tam];
+
+            // Lee los datos de los alumnos, los valida y los almacena en un arreglo
+            for (int i = 0; i < tam; i++)
+            {
+                Console.WriteLine("Nombre del alumno {0}:", i);
+                nom = Console.ReadLine();
+                Console.WriteLine("Apellido paterno del alumno {0}:", i);
+                ap_pa = Console.ReadLine();
+                Console.WriteLine("Apellido materno del alumno {0}", i);
+                ap_ma = Console.ReadLine();
+                Console.WriteLine("Calificacion del proyecto");
+                calif[0] = Convert.ToDouble(Console.ReadLine());
+                if(calif[0] < 0 || calif[0] > 10)
+                {
+                    do
+                    {
+                        Console.WriteLine("Calificacion debe ser del 1 - 10");
+                        calif[0] = Convert.ToDouble(Console.ReadLine());
+                    } while (calif[0] > 10 || calif[0] < 0);
+
+                }
+                Console.WriteLine("Cuantas tareas entrego el alumno {0}", i);
+                calif[1] = Convert.ToDouble(Console.ReadLine());
+                if (calif[1] < 0 || calif[1] > 5)
+                {
+                    do
+                    {
+                        Console.WriteLine("Las tareas totales fueron 5, no puede pasar ese numero");
+                        calif[1] = Convert.ToDouble(Console.ReadLine());
+                    } while (calif[1] > 5 || calif[1] < 0);
+
+                }
+                Console.WriteLine("Participaciones del alumno {0}", i);
+                calif[2] = Convert.ToDouble(Console.ReadLine());
+                if (calif[2] < 0)
+                {
+                    do
+                    {
+                        Console.WriteLine("No puede haber participaciones negativas");
+                        calif[2] = Convert.ToDouble(Console.ReadLine());
+                    } while (calif[2] < 0);
+                }
+                alumnos[i] = new Alumno(nom, ap_pa, ap_ma, calif);
+            }
+
+            foreach(Alumno alum in alumnos)
+            {
+                Console.WriteLine(alum.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
