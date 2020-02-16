065fe887be3443589e72183cb047adbd45a2301d
AngelLS24 <angls2495@gmail.com>
2020-02-15 23:19:47 -0600

    Ejercicio de la Consola

---
+++ Tareas/Consola/Consola/Comandos.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Microsoft.VisualBasic;
+
+namespace Consola
+{
+    class Comandos
+    {
+        // Funcion que realiza la funcion de cd
+        public static bool CdComand(string path)
+        {
+            // Obtiene la direccion del directorio acual
+            string dir = Directory.GetCurrentDirectory();
+            // valida si se establece un directorio 
+            if (path != "..")
+            {
+                try
+                {
+                    // Intenta cambiar al directorio ingresado
+                    Directory.SetCurrentDirectory(dir + @"\" + path);
+                    // Retorna true
+                    return true;
+                }
+                catch (Exception)
+                {
+                    // Si no puede cambiar al directorio despliega un mensaje
+                    Console.WriteLine("El directorio '{0}' no existe", path);
+                    // Retorna false
+                    return false;
+                }
+            }
+            // Si el comando es cd ..
+            else
+            {
+                // Cambia de directorio un nivel arriba
+                FileSystem.ChDir("..");
+                // Retorna true
+                return true;
+            }
+        }
+        // Funcion que imprime el historial
+        public static void HistoryCommand(List<string> history)
+        {
+            // Contador que nos dice el numero del comando
+            int contador = 0;
+            // Recorre la lista del historial
+            foreach(var item in history)
+            {
+                // Aumenta el contador
+                contador++;
+                // Imprime el contador con el comando guardado
+                Console.WriteLine("{0}: {1}", contador, item);
+            }
+        }
+        // Funcion que nos ayuda a agregar valores a una lista
+        public static void AddList(string tipo, string[] arr, ref List<string> contenido)
+        {
+            // Variable que nos ayuda a guardar el directorio/archivo
+            string[] con;
+            // Recorre el arreglo que recibe con los directorios/archivos
+            foreach (var a in arr)
+            {
+                // Obtiene el nombre del directorio/archivo
+                con = a.Split(@"\");
+                // Agrega el directorio/archivo a la lista
+                contenido.Add(tipo + con[^1]);
+            }
+        }
+        // Funcion que lista los directorios/archivos
+        public static void DirComand(string dir)
+        {
+            // Obtiene todos los archivos del directorio actual
+            string[] archivos = Directory.GetFiles(dir);
+            // Obtiene todos los directorios del directorio actual
+            string[] directorios = Directory.GetDirectories(dir);
+            // Lista que guardara todos los elementos listados
+            List<string> contenido = new List<string>();
+            // Llama a la funcion para agregar los elementos a la lista
+            AddList("<FILE> ", archivos, ref contenido);
+            // Llama a la funcion para agregar los elementos a la lista
+            AddList("<DIR> ", directorios, ref contenido);
+            // Ordena la lista
+            contenido.Sort();
+            // Recorre la lista e imprime el el elemento de la lista
+            foreach (var c in contenido)
+                Console.WriteLine(c);
+        }
+        // Funcion que nos ayuda a validar los comandos con sus argumentos
+        public static string[] ValidaComando(string comando, int tam)
+        {
+            // Variable que nos ayuda a almacenar 
+            string[] palabras;
+            // Variable que almacena un arreglo con las palabras del comando
+            string[] salida = new string[tam];
+            // Variable que nos ayuda de contador
+            int cont = 0;
+            // Verifica si el comando tiene comillas
+            if (comando.Contains('"'))
+                // Separa el comando por comillas
+                palabras = comando.Split('"');
+            else
+                // Si no tiene comillas lo separa por espacio
+                palabras = comando.Split(" ");
+            // Realiza un cliclo, este ciclo quita los espacios en blanco de las palabras separadas del comando
+            for(int i = 0; i < palabras.Length; i++)
+                palabras[i] = palabras[i].TrimEnd(' ');
+            
+            try
+            {
+                // Recorre las palabras almacenadas
+                foreach (var p in palabras)
+                {
+                    // Verifica si no es un espacio en blanco
+                    if (!p.Equals(""))
+                    {
+                        // Si no es un espacio en blanco lo agrega al arreglo de salida
+                        salida[cont] = p;
+                        // Incrementa el contador
+                        cont++;
+                    }
+                }
+            }
+            // Si el comando tiene mas argumentos de los requeridos lanza una excepcion
+            catch (IndexOutOfRangeException)
+            {
+                // Asigna un mensaje en el primer elemento del arreglo de salida
+                salida[0] = "No se puede ingresar mas argumentos de los admitidos: " + (tam - 1);
+            }
+            // Retorna el arreglo de salida
+            return salida;
+        }
+        // Funcion que crea archivos
+        public static bool TouchComand(string path)
+        {
+            // Verifica que el archivo no exista
+            if(!File.Exists(path))
+            { 
+                // Crea el archivo
+                using(FileStream fs = File.Create(path))
+                    // Cierra el archivo
+                    fs.Close();
+                // Retorna verdader
+                return true;
+            }
+            // Si el archivo existe
+            else
+            {
+                // Despliega un mensaje
+                Console.WriteLine("El archivo '{0}' ya existe", path);
+                // Retorna falso
+                return false;
+            }
+        }
+        // Funcion que mueve un archivo de carpeta
+        public static bool MoveComand(string origen, string destino)
+        {
+            try
+            {
+                // Intenta mover un archivo de un origen a destino
+                Directory.Move(origen, destino);
+                // Retorna true
+                return true;
+            }
+            // Si no puede moverlo lanza una excepcion
+            catch (Exception)
+            {
+                // Despliega el mensaje 
+                Console.WriteLine("Direccion invalida");
+                // Retorna false
+                return false;
+            }
+        }
+    }
+}
---
+++ Tareas/Consola/Consola/Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Consola
+{
+    class Program
+    {
+        // Variable que guarda una cadena de la forma C:\Users\user\
+        public static string path = @"C:\Users\" + Environment.UserName + @"\";
+        static void Main()
+        {
+            // Lista que guardara los comandos ejecutados
+            List<string> history = new List<string>();
+            // Variable que almacena un valor booleano, esta variable mantendra el programa ejecutandose
+            bool continua = true;
+            // Variable que va a almacenar lo introducido por el usuario
+            string comando;
+            // Este bloque permite establecer la direccion
+            try
+            {
+                // Se optiene el directorio Documents o Documentos
+                string[] dir = Directory.GetDirectories(path, "Document*s?", SearchOption.TopDirectoryOnly); 
+                // Si solo existe un elemento en la variable dir, usara ese elemento para establecer el directorio
+                if(dir.Length == 1)
+                    Directory.SetCurrentDirectory(dir[0]);
+                // Si existen dos carpetas, Documentos y Documents se establece la direccion guardada en path
+                else
+                    Directory.SetCurrentDirectory(path);
+            }
+            // Si no encuentra ninguna de las direcciones establecidas arriba, manda un error
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("No se encontro del directorio {0}", path);
+            }
+
+            // Este bloque ejecuta todo el programa
+            do
+            {
+                // Imprime la direccion actual del directorio
+                Console.Write("{0}> ", Directory.GetCurrentDirectory());
+                // Lee el comando
+                comando = Console.ReadLine();
+                // Separa el comando por espacios
+                string[] palabras = comando.Split(" ");
+                // Variable que nos ayuda a validar los comandos
+                string[] valida;
+                // Obtiene el comando y lo evalua
+                switch (palabras[0])
+                {
+                    case "dir":
+                        // Valida que el comando escrito solo reciba 2 argumentos
+                        valida = Comandos.ValidaComando(comando, 2);
+                        // Si la primera palabra es el comando se ejecuta lo siguiente
+                        if (valida[0].Equals("dir"))
+                        {
+                            try
+                            {
+                                // Obtiene el directorio actual
+                                string dir = Directory.GetCurrentDirectory();
+                                // Verifica si recibe algun argumento
+                                if (String.IsNullOrEmpty(valida[1]))
+                                    // Llama a la funcion que cambia de directorio
+                                    Comandos.DirComand(dir);
+                                // Si recibe una carpeta como argumento
+                                else
+                                    // Se ejecuta el comando dir sobre el directorio ingresado
+                                    Comandos.DirComand(dir + @"\" + valida[1]);
+                                // Agrega el comando a la lista del historial
+                                history.Add(comando);
+                            }
+                            // Si no existe el directorio que se pasa como argumento manda una excepcion
+                            catch (DirectoryNotFoundException)
+                            {
+                                Console.WriteLine("El directorio '{0}' no existe", valida[1]);
+                            }
+                        }
+                        // Si no es el comando, es un mensjare y se imprime
+                        else
+                            Console.WriteLine(valida[0]);
+                        // Termina la ejecucion del bloque
+                        break;
+                    case "cd":
+                        // Valida que el comando escrito solo reciba 2 argumentos
+                        valida = Comandos.ValidaComando(comando, 2);
+                        // Si la primera palabra es el comando se ejecuta lo siguiente
+                        if (valida[0].Equals("cd"))
+                        {
+                            // Verifica si recibe algun argumento que no sea nulo
+                            if (!String.IsNullOrEmpty(valida[1]))
+                            { 
+                                // Llama a la funcion que retornara un valor booleano
+                                if (Comandos.CdComand(valida[1]))
+                                {
+                                    // Verifica si lo ingresado tiene comillas
+                                    if (valida[1].Contains(" "))
+                                        // Agrega el comando a la lista del historial
+                                        history.Add(valida[0] + " \"" + valida[1] + "\"");
+                                    else
+                                        // Agrega el comando a la lista del historial
+                                        history.Add(valida[0] + " " + valida[1]);
+                                }
+                            }else
+                                Console.WriteLine("Se debe especificar el directorio al que se quiere cambiar");
+                        }
+                        else
+                            Console.WriteLine(valida[0]);
+                        // Termina la ejecucion del bloque
+                        break;
+                    case "touch":
+                        // Valida que el comando escrito solo reciba 2 argumentos
+                        valida = Comandos.ValidaComando(comando, 2);
+                        // Si la primera palabra es el comando se ejecuta lo siguiente
+                        if (valida[0].Equals("touch"))
+                        {
+                            // Llama a la funcion que retornara un valor booleano
+                            if (Comandos.TouchComand(valida[1]))
+                                // Agrega el comando a la lista del historial
+                                history.Add(valida[0] + " " + valida[1]);
+                        }
+                        else
+                            Console.WriteLine(valida[0]);
+                        // Termina la ejecucion del bloque
+                        break;
+                    case "move":
+                        // Valida si el comando tiene comillas, las comillas son ingresadas para las 
+                        // carpetas con espacios
+                        if (comando.Contains('"'))
+                        {
+                            // Valida que el comando escrito solo reciba 2 argumentos
+                            valida = Comandos.ValidaComando(comando, 2);
+                            // Separara el valor de valida[0] por los espacios
+                            string[] pal = Comandos.ValidaComando(valida[0], 2);
+                            // Si la primera palabra es el comando se ejecuta lo siguiente
+                            if (pal[0].Equals("move"))
+                            {
+                                // Llama a la funcion que retornara un valor booleano
+                                if (Comandos.MoveComand(pal[1], valida[1]))
+                                    // Agrega el comando a la lista del historial
+                                    history.Add(pal[0] + " " + pal[1] + " \"" + valida[1] + "\"");
+                            }
+                            else
+                                Console.WriteLine(valida[0]);
+                        }
+                        else
+                        {
+                            // Valida que el comando escrito solo reciba 3 argumentos
+                            valida = Comandos.ValidaComando(comando, 3);
+                            // Si la primera palabra es el comando se ejecuta lo siguiente
+                            if (valida[0].Equals("move"))
+                            {
+                                // Llama a la funcion que retornara un valor booleano
+                                if (Comandos.MoveComand(valida[1], valida[2]))
+                                    // Agrega el comando a la lista del historial
+                                    history.Add(valida[0] + " " + valida[1] + " " + valida[2]);
+                            }
+                            else
+                                Console.WriteLine(valida[0]);
+                        }
+                        // Termina la ejecucion del bloque
+                        break;
+                    case "history":
+                        // Agrega el comando a la lista del historial
+                        history.Add(palabras[0]);
+                        // Llama a la funcion que imprime el historial de comandos 
+                        Comandos.HistoryCommand(history);
+                        break;
+                    case "cls":
+                        // Agrega el comando a la lista del historial
+                        history.Add(palabras[0]);
+                        // Limpia la pantalla 
+                        Console.Clear();
+                        // Termina la ejecucion del bloque
+                        break;
+                    case "exit":
+                        // La bandera se vuelve false y con eso termina el programa
+                        continua = false;
+                        // Termina la ejecucion del bloque
+                        break;
+                    default:
+                        Console.WriteLine("Comando '{0}' no existe", palabras[0]);
+                        // Termina la ejecucion del bloque
+                        break;
+                }
+            } while (continua);
+        }
+    }
+}
