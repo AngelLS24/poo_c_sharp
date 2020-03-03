3b52b74d22392b709577227ef391e8d4bff623ec
ACMEproductions <acmeproductionsgm@gmail.com>
2020-03-03 12:38:18 -0600

    Corrección interfaces: Parametros de evaluación

---
+++ Escuela/Escuela/Parametros.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Escuela
+{
+    partial class Parametros
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txBanner = new System.Windows.Forms.Label();
+            this.txParam = new System.Windows.Forms.Label();
+            this.txProy = new System.Windows.Forms.Label();
+            this.txExam = new System.Windows.Forms.Label();
+            this.txTarea = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbProyectoPercent = new System.Windows.Forms.TextBox();
+            this.tbExamPercent = new System.Windows.Forms.TextBox();
+            this.tbTareaPercent = new System.Windows.Forms.TextBox();
+            this.tbProyectosCant = new System.Windows.Forms.TextBox();
+            this.tbExamCant = new System.Windows.Forms.TextBox();
+            this.tbTareasCant = new System.Windows.Forms.TextBox();
+            this.btParam = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txBanner
+            // 
+            this.txBanner.AutoSize = true;
+            this.txBanner.Location = new System.Drawing.Point(36, 40);
+            this.txBanner.Name = "txBanner";
+            this.txBanner.Size = new System.Drawing.Size(135, 17);
+            this.txBanner.TabIndex = 0;
+            this.txBanner.Text = "Bienvenido profesor";
+            // 
+            // txParam
+            // 
+            this.txParam.AutoSize = true;
+            this.txParam.Location = new System.Drawing.Point(39, 83);
+            this.txParam.Name = "txParam";
+            this.txParam.Size = new System.Drawing.Size(404, 17);
+            this.txParam.TabIndex = 1;
+            this.txParam.Text = "Ingrese los parámetros de evaluacion de los siguientes rubros:";
+            // 
+            // txProy
+            // 
+            this.txProy.AutoSize = true;
+            this.txProy.Location = new System.Drawing.Point(39, 165);
+            this.txProy.Name = "txProy";
+            this.txProy.Size = new System.Drawing.Size(71, 17);
+            this.txProy.TabIndex = 2;
+            this.txProy.Text = "Proyectos";
+            // 
+            // txExam
+            // 
+            this.txExam.AutoSize = true;
+            this.txExam.Location = new System.Drawing.Point(39, 214);
+            this.txExam.Name = "txExam";
+            this.txExam.Size = new System.Drawing.Size(73, 17);
+            this.txExam.TabIndex = 3;
+            this.txExam.Text = "Examenes";
+            // 
+            // txTarea
+            // 
+            this.txTarea.AutoSize = true;
+            this.txTarea.Location = new System.Drawing.Point(39, 259);
+            this.txTarea.Name = "txTarea";
+            this.txTarea.Size = new System.Drawing.Size(53, 17);
+            this.txTarea.TabIndex = 4;
+            this.txTarea.Text = "Tareas";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(202, 142);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Porcentaje";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(372, 142);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Cantidad";
+            // 
+            // tbProyectoPercent
+            // 
+            this.tbProyectoPercent.Location = new System.Drawing.Point(188, 162);
+            this.tbProyectoPercent.MaxLength = 3;
+            this.tbProyectoPercent.Name = "tbProyectoPercent";
+            this.tbProyectoPercent.Size = new System.Drawing.Size(100, 22);
+            this.tbProyectoPercent.TabIndex = 7;
+            // 
+            // tbExamPercent
+            // 
+            this.tbExamPercent.Location = new System.Drawing.Point(188, 211);
+            this.tbExamPercent.MaxLength = 3;
+            this.tbExamPercent.Name = "tbExamPercent";
+            this.tbExamPercent.Size = new System.Drawing.Size(100, 22);
+            this.tbExamPercent.TabIndex = 8;
+            // 
+            // tbTareaPercent
+            // 
+            this.tbTareaPercent.Location = new System.Drawing.Point(188, 256);
+            this.tbTareaPercent.MaxLength = 3;
+            this.tbTareaPercent.Name = "tbTareaPercent";
+            this.tbTareaPercent.Size = new System.Drawing.Size(100, 22);
+            this.tbTareaPercent.TabIndex = 9;
+            // 
+            // tbProyectosCant
+            // 
+            this.tbProyectosCant.Location = new System.Drawing.Point(355, 162);
+            this.tbProyectosCant.MaxLength = 2;
+            this.tbProyectosCant.Name = "tbProyectosCant";
+            this.tbProyectosCant.Size = new System.Drawing.Size(100, 22);
+            this.tbProyectosCant.TabIndex = 10;
+            // 
+            // tbExamCant
+            // 
+            this.tbExamCant.Location = new System.Drawing.Point(355, 209);
+            this.tbExamCant.MaxLength = 2;
+            this.tbExamCant.Name = "tbExamCant";
+            this.tbExamCant.Size = new System.Drawing.Size(100, 22);
+            this.tbExamCant.TabIndex = 11;
+            // 
+            // tbTareasCant
+            // 
+            this.tbTareasCant.Location = new System.Drawing.Point(355, 254);
+            this.tbTareasCant.MaxLength = 2;
+            this.tbTareasCant.Name = "tbTareasCant";
+            this.tbTareasCant.Size = new System.Drawing.Size(100, 22);
+            this.tbTareasCant.TabIndex = 12;
+            // 
+            // btParam
+            // 
+            this.btParam.Location = new System.Drawing.Point(205, 314);
+            this.btParam.Name = "btParam";
+            this.btParam.Size = new System.Drawing.Size(83, 28);
+            this.btParam.TabIndex = 13;
+            this.btParam.Text = "Continuar";
+            this.btParam.UseVisualStyleBackColor = true;
+            this.btParam.Click += new System.EventHandler(this.btParam_Click);
+            // 
+            // Parametros
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 366);
+            this.Controls.Add(this.btParam);
+            this.Controls.Add(this.tbTareasCant);
+            this.Controls.Add(this.tbExamCant);
+            this.Controls.Add(this.tbProyectosCant);
+            this.Controls.Add(this.tbTareaPercent);
+            this.Controls.Add(this.tbExamPercent);
+            this.Controls.Add(this.tbProyectoPercent);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txTarea);
+            this.Controls.Add(this.txExam);
+            this.Controls.Add(this.txProy);
+            this.Controls.Add(this.txParam);
+            this.Controls.Add(this.txBanner);
+            this.Name = "Parametros";
+            this.Text = "Parametros";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txBanner;
+        private System.Windows.Forms.Label txParam;
+        private System.Windows.Forms.Label txProy;
+        private System.Windows.Forms.Label txExam;
+        private System.Windows.Forms.Label txTarea;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbProyectoPercent;
+        private System.Windows.Forms.TextBox tbExamPercent;
+        private System.Windows.Forms.TextBox tbTareaPercent;
+        private System.Windows.Forms.TextBox tbProyectosCant;
+        private System.Windows.Forms.TextBox tbExamCant;
+        private System.Windows.Forms.TextBox tbTareasCant;
+        private System.Windows.Forms.Button btParam;
+    }
+}
---
+++ Escuela/Escuela/Parametros.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Escuela
+{
+    public partial class Parametros : Form
+    {
+        //Variables de evaluacion
+        int examenPercent;
+        int examenCant;
+        int proyectoPercent;
+        int proyectoCant;
+        int tareasPercent;
+        int tareasCant;
+
+        //Para validación de datos
+        Boolean flag = true;
+        public Parametros()
+        {
+            InitializeComponent();
+        }
+
+        private void btParam_Click(object sender, EventArgs e)
+        {
+            //Guardamos datos del porcentaje del examen
+            try
+            {
+                Convert.ToInt32(tbExamPercent.Text);    //Si falla la conversion
+                //Validacion de datos
+                if (Convert.ToInt32(tbExamPercent.Text) > 0 && Convert.ToInt32(tbExamPercent.Text) < 100)
+                    examenPercent = Convert.ToInt32(tbExamPercent.Text);
+                else
+                    flag = false;
+            }
+            catch (FormatException) { flag = false; }
+
+            //Guardamos datos de la cantidad del examen
+            try
+            {
+                Convert.ToInt32(tbExamCant.Text);       //Si falla la conversion
+                //Validacion de datos
+                if (Convert.ToInt32(tbExamCant.Text) > 0)
+                    examenCant = Convert.ToInt32(tbExamCant.Text);
+                else
+                    flag = false;
+            }
+            catch (FormatException) { flag = false; }
+
+            //Guardamos datos del porcentaje del proyecto
+            try
+            {
+                Convert.ToInt32(tbProyectoPercent.Text);    //Si falla la conversion
+                //Validacion de datos
+                if (Convert.ToInt32(tbProyectoPercent.Text) > 0 && Convert.ToInt32(tbProyectoPercent.Text) < 100)
+                    proyectoPercent = Convert.ToInt32(tbProyectoPercent.Text);
+                else
+                    flag = false;
+            }
+            catch (FormatException) { flag = false; }
+
+            //Guardamos datos de la cantidad del proyecto
+            try
+            {
+                Convert.ToInt32(tbProyectosCant.Text);       //Si falla la conversion
+                //Validacion de datos
+                if (Convert.ToInt32(tbProyectosCant.Text) > 0)
+                    proyectoCant = Convert.ToInt32(tbProyectosCant.Text);
+                else
+                    flag = false;
+            }
+            catch (FormatException) { flag = false; }
+
+            //Guardamos datos del porcentaje de las tareas
+            try
+            {
+                Convert.ToInt32(tbTareaPercent.Text);    //Si falla la conversion
+                //Validacion de datos
+                if (Convert.ToInt32(tbTareaPercent.Text) > 0 && Convert.ToInt32(tbTareaPercent.Text) < 100)
+                    tareasPercent = Convert.ToInt32(tbTareaPercent.Text);
+                else
+                    flag = false;
+            }
+            catch (FormatException) { flag = false; }
+
+            //Guardamos datos de la cantidad de tareas
+            try
+            {
+                Convert.ToInt32(tbTareasCant.Text);       //Si falla la conversion
+                //Validacion de datos
+                if (Convert.ToInt32(tbTareasCant.Text) > 0)
+                    tareasCant = Convert.ToInt32(tbTareasCant.Text);
+                else
+                    flag = false;
+            }
+            catch (FormatException) { flag = false; }
+
+            //Lanzamos la siguiente ventana si los valores son válidos
+            if (flag)
+            {
+                //Lanzamos la form del menu
+                //Menu Menu = new Menu();
+                //Menu.Visible = true;
+                //Cerramos la ventana actual
+                this.Dispose();
+            }
+            else
+            {
+                //Clear textboxes and launch message
+
+            }
+        }
+    }
+}
